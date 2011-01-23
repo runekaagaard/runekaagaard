5dd17a42e48aea5a36b1af2c0ef67b5a4fd106f9
rumi.kg <rumi.kg@5f7993f6-7781-11de-adf2-cbd3851e0ccf>
2011-01-23 21:48:25 +0000

    Creating a base class for ajaxselects.

--- trunk/django-ajax-selects/test_project/test_app/admin.py
+++ trunk/django-ajax-selects/test_project/test_app/admin.py
@@ -20,38 +20,37 @@ class MusicianAdmin(BaseAdmin): pass
     
 from django.db.models import Q
 
-class MusicianLookup(object):
-
-    def get_query(self,q,request):
-        """ return a query set.  you also have access to request.user if needed """
-        return Musician.objects.filter(Q(first_name__icontains=q))
-
+class AjaxSelectLookup(object):
+    def get_query_by_id(self, id, request):
+        return self.model.objects.get(pk=id)
+    
     def format_result(self, obj):
         """ the search results display in the dropdown menu.  may contain html and multiple-lines. will remove any |  """
         return unicode(obj)
 
     def format_item(self, obj):
         """ the display of a currently selected object in the area below the search box. html is OK """
-        return unicode(obj)
+        return "%s - extra" % unicode(obj)
 
     def get_objects(self, ids):
         """ given a list of ids, return the objects ordered as you would like them on the admin page.
             this is for displaying the currently selected items (in the case of a ManyToMany field)
         """
-        return Musician.objects.filter(pk__in=ids)
-
-def register(model, channel):
-    meta = str(model._meta)
-    parts = meta.split(".")
-    channel_name = str(channel.__name__)
-    settings.AJAX_LOOKUP_CHANNELS[parts[1]] = (
-        'test_app.admin', channel_name)
+        return self.model.objects.filter(pk__in=ids)
     
-print settings.AJAX_LOOKUP_CHANNELS
+    def get_query(self, q, request):
+        """ return a query set. you also have access to request.user if needed """
+        qs = Q()
+        for field in self.search_fields:
+            qs = qs | Q(**{'%s__icontains' % field: q})
+        return self.model.objects.filter(qs)
+    
+class MusicianLookup(AjaxSelectLookup):
+    model = Musician
+    search_fields = ('first_name', 'last_name')
+
 class AlbumAdmin(BaseAdmin):
     form = make_ajax_form(Album, dict(musician='musician'))
-
-register(Musician, MusicianLookup)
 
 admin.site.register(Musician, MusicianAdmin)
 admin.site.register(Album, AlbumAdmin)
