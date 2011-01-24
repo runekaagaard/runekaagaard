9c799913501e323ffdc1ed1bce0f0ffc15a418bb
rumi.kg <rumi.kg@5f7993f6-7781-11de-adf2-cbd3851e0ccf>
2011-01-24 13:42:59 +0000

    Adding register function that removes the need of stuff in the settings file. Hacky.

--- django-ajax-selects/test_project/test_app/admin.py
+++ django-ajax-selects/test_project/test_app/admin.py
@@ -3,6 +3,8 @@ from ajax_select import make_ajax_form
 from ajax_select.admin import AjaxSelectAdmin
 from models import Musician, Album
 from django.conf import settings
+from django.db.models import Q
+from random import randint
 
 class BaseAdmin(AjaxSelectAdmin):
     class Media:
@@ -18,11 +20,9 @@ class BaseAdmin(AjaxSelectAdmin):
 
 class MusicianAdmin(BaseAdmin): pass
     
-from django.db.models import Q
-
 class AjaxSelectLookup(object):
     def get_query_by_id(self, id, request):
-        return self.model.objects.get(pk=id)
+        return self.related_model.objects.get(pk=id)
     
     def format_result(self, obj):
         """ the search results display in the dropdown menu.  may contain html and multiple-lines. will remove any |  """
@@ -36,21 +36,33 @@ class AjaxSelectLookup(object):
         """ given a list of ids, return the objects ordered as you would like them on the admin page.
             this is for displaying the currently selected items (in the case of a ManyToMany field)
         """
-        return self.model.objects.filter(pk__in=ids)
+        return self.related_model.objects.filter(pk__in=ids)
     
     def get_query(self, q, request):
         """ return a query set. you also have access to request.user if needed """
         qs = Q()
-        for field in self.search_fields:
+        for field in self.related_search_fields:
             qs = qs | Q(**{'%s__icontains' % field: q})
-        return self.model.objects.filter(qs)
-    
+        return self.related_model.objects.filter(qs)
+
+def register(lookup, admin=None):
+    channel_name = 'lookup_%d' % randint(0,999999)
+    settings.AJAX_LOOKUP_CHANNELS[channel_name] = (lookup.__module__, 
+                                                        lookup.__name__)
+    if admin is not None:
+        admin.form = make_ajax_form(lookup.model, 
+                                    {lookup.field_name: channel_name})
+
+class AlbumAdmin(BaseAdmin): pass
+
 class MusicianLookup(AjaxSelectLookup):
-    model = Musician
-    search_fields = ('first_name', 'last_name')
-
-class AlbumAdmin(BaseAdmin):
-    form = make_ajax_form(Album, dict(musician='musician'))
-
+    model = Album
+    field_name = 'musician'
+    related_model = Musician
+    related_search_fields = ('first_name', 'last_name')
+register(MusicianLookup, AlbumAdmin)
+    
+    
+    
 admin.site.register(Musician, MusicianAdmin)
 admin.site.register(Album, AlbumAdmin)
